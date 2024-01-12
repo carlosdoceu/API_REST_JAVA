25d2ad8609764dce6e1d49445a6915c2ce476615
Carlos Omar <carlos.omar@tjro.jus.br>
2024-01-12 12:37:21 -0400

    Adicionando listagem com ordenacao direto no controller utilizando jpa nativo

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,11 +1,15 @@
 package med.voll.api.controller;
 
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,16 +25,14 @@ public class MedicoController {
 
     @PostMapping("/cadastroMedicos")
     @Transactional /*por ser um insert, precisa ter transação ativa no db*/
-    public void cadastroMedico(@RequestBody DadosCadastroMedico dadosCadastroMedico){
+    public void cadastroMedico(@RequestBody  @Valid DadosCadastroMedico dadosCadastroMedico){
         medicoRepository.save(new Medico(dadosCadastroMedico));
-
 
     }
 
-
     @GetMapping("/listaMedicos")
-    public List<DadosListagemMedicos> listaMedicos(){
-        return medicoRepository.findAll().stream().map(DadosListagemMedicos::new).toList();
+    public Page<DadosListagemMedicos> listaMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable){
+        return medicoRepository.findAll(pageable).map(DadosListagemMedicos::new); /* DadosListagemMedicos::new - chama o constructor do DTO de DadosListagemMEdicos  */
     }
 
 }
